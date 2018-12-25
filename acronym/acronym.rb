82800a080b8d5a0dfd17b86042a96a17d5e0276f
Steven Bowen <sdbowen.git@gmail.com>
2018-12-25 07:24:49 -0600

    Refactor solution to use regexp

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,16 +1,6 @@
-class String
-  def initial
-    self[0, 1]
-  end
-end
-
 class Acronym
+  # Return first letter of each word
   def self.abbreviate(string)
-    # Remove dashes from string
-    string.gsub!('-',' ')
-    # Split string into words array
-    words = string.split
-    # Get first letter of each word
-    return words.map(&:initial).join.upcase
+    return string.scan(/\b\w/).join.upcase
   end
 end
