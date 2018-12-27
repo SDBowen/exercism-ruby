ae4b2098e2ead159fb55b1c35f34eaf2af988975
Steven Bowen <sdbowen.git@gmail.com>
2018-12-27 13:13:00 -0700

    Submit solution

---
+++ matrix/matrix.rb
@@ -0,0 +1,31 @@
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(string)
+    @rows = create_rows(string)
+    @columns = create_columns
+  end
+
+  private
+
+  def create_rows(text)
+    strings_matrix = text.split("\n").map(&:split)
+
+    strings_matrix.map do |array|
+      array.map(&:to_i)
+    end
+  end
+
+  def create_columns
+    matrix_columns = []
+
+    i = 0
+    while i < rows.length
+      matrix_columns << rows.map { |array| array[i] }
+
+      i += 1
+    end
+
+    matrix_columns
+  end
+end
