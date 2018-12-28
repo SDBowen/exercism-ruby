7b10e1084c92e1658c005a8c39635030783c779e
Steven Bowen <sdbowen.git@gmail.com>
2018-12-27 17:20:15 -0700

    Add transpose method and refactor create_rows

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -3,29 +3,16 @@ class Matrix
 
   def initialize(string)
     @rows = create_rows(string)
-    @columns = create_columns
+    @columns = rows.transpose
   end
 
   private
 
   def create_rows(text)
-    strings_matrix = text.split("\n").map(&:split)
+    strings_matrix = text.lines.map(&:split)
 
     strings_matrix.map do |array|
       array.map(&:to_i)
     end
-  end
-
-  def create_columns
-    matrix_columns = []
-
-    i = 0
-    while i < rows.length
-      matrix_columns << rows.map { |row| row[i] }
-
-      i += 1
-    end
-
-    matrix_columns
   end
 end
