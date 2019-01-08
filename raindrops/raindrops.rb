b0f03cf7214e7585393dbbbe9d575db85a515b6d
Steven Bowen <sdbowen.git@gmail.com>
2019-01-08 15:14:40 -0700

    Add raindrops solution with loop

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -2,12 +2,9 @@ class Raindrops
   def self.convert(number)
     rain_sounds = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
 
-    message = ''
-
-    rain_sounds.each do |factor, sound|
-      message << sound if (number % factor).zero?
-    end
-
+    message = rain_sounds.map { |factor, sound|
+       sound if (number % factor).zero? }.join
+      
     message << number.to_s if message.empty?
 
     message
