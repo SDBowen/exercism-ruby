0be81ee15f31c9913b958ce2de47f0e4ace5490b
Steven Bowen <sdbowen.git@gmail.com>
2019-01-20 12:55:11 -0700

    Refactor for readability and add argument error text

--- series/series.rb
+++ series/series.rb
@@ -1,15 +1,17 @@
 class Series
+  attr_reader :digits
+  
   def initialize(digits)
     @digits = digits
   end
 
   def slices(num_of_slices)
-    raise ArgumentError if num_of_slices > @digits.length
+    raise ArgumentError.new('Slices is greater than digits available.') if num_of_slices > digits.length
 
     sliced_series = []
 
-    @digits.chars.each_with_index do |_char, index|
-      current_slice = @digits.slice(index, num_of_slices)
+    digits.chars.each_with_index do |_char, index|
+      current_slice = digits.slice(index, num_of_slices)
 
       sliced_series << current_slice if current_slice.length == num_of_slices
     end
